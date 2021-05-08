2f36cd9397f65fdb410ae45e76001b7db639f8c4
Audris Dobrikas <audrisdob@gmail.com>
2021-05-08 07:36:10 +0300

    commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,13 +9,13 @@
   <title>AdminLTE 3 | Starter</title>
 
   <!-- Font Awesome Icons -->
-  <link rel="stylesheet" href="{{ asset('backend/plugins/fontawesome-free/css/all.min.css') }}">
+  <link rel="stylesheet" href="../backend/plugins/fontawesome-free/css/all.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="{{ asset('backend/dist/css/adminlte.min.css') }}">
+  <link rel="stylesheet" href="../backend/dist/css/adminlte.min.css">
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 
-  <link rel="stylesheet" type="text/css" href="{{ asset('backend/plugins/toastr/toastr.min.css') }}">
+  <link rel="stylesheet" type="text/css" href="../backend/plugins/toastr/toastr.min.css">
 
   <livewire:styles />
 
@@ -55,13 +55,13 @@
 <!-- REQUIRED SCRIPTS -->
 
 <!-- jQuery -->
-<script src="{{ asset('backend/plugins/jquery/jquery.min.js') }}"></script>
+<script src="../backend/plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
-<script src="{{ asset('backend/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="../backend/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
-<script src="{{ asset('backend/dist/js/adminlte.min.js') }}"></script>
+<script src="../backend/dist/js/adminlte.min.js"></script>
 
-<script type="text/javascript" src="{{ asset('backend/plugins/toastr/toastr.min.js') }}"></script>
+<script type="text/javascript" src="../backend/plugins/toastr/toastr.min.js"></script>
 
 <script>
   $(document).ready( function() {
